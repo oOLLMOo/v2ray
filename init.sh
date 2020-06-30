c6cd466f6e6fb9b63083828734f038266d0721d5
oOLLMOo <timmyl16@163.com>
2020-06-30 10:53:27 +0800

    init 1.0

--- init.sh
+++ init.sh
@@ -6,8 +6,12 @@
 #bash <(curl -sL https://raw.githubusercontent.com/oOLLMOo/v2ray/master/test.sh) $((RANDOM%50000+500))
 
 #使用500-50000随机数端口
-bash < (curl -sL https://raw.githubusercontent.com/oOLLMOo/v2ray/master/ubuntu_install_v2ray_llm.sh) $((RANDOM%50000+500))
+bash <(curl -sL https://raw.githubusercontent.com/oOLLMOo/v2ray/master/ubuntu_install_v2ray_llm.sh) $((RANDOM%50000+500))
 
 #日志分析依赖和脚本安装
 apt update
 apt-get install -y python3
+cd /var/log/v2ray
+wget https://raw.githubusercontent.com/oOLLMOo/v2ray/master/analy.sh
+wget https://raw.githubusercontent.com/oOLLMOo/v2ray/master/analy_json.py
+cd -
